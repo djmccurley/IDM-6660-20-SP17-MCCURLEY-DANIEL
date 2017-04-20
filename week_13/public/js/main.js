6a4808576d56e534f2e7c334a2f87736d599a01b
Dan McCurley <djmccurley@gmail.com>
2017-04-20 12:44:39 -0500

    added functions to output entries in individual divs

--- week_13/public/js/main.js
+++ week_13/public/js/main.js
@@ -17,8 +17,34 @@ $(document).ready(function() {
 		searchSettings["url"] = urlBase + searchTerm;
 		console.log(searchSettings["url"]);
 	  $.ajax(searchSettings).done(function(response) {
-	  console.log(response);
-	  $("#output").text(response);
+		  console.log(response);
+		  processEntries(response);
 		});
-	}	
+	}
+
+	function processEntries(response) {
+		for (i=0; i<response[1].length; i++) {
+			var title = response[1][i];
+			var description = response[2][i];
+			var link = response[3][i];
+			var thisArticle = new createEntry(title, description, link);
+			appendEntry(thisArticle);
+	}
+}
+
+function createEntry(title, description, link) {
+	this.articleTitle = "<h2>" + title + "</h2>";
+	this.articleDescription = "<p>" + description + "</p>";
+	this.articleLink = "<a href='" + link + "' target='_blank'>" + link + "</a>";
+}
+
+function appendEntry(thisArticle) {
+	var newDiv = document.createElement("div");
+	newDiv.innerHTML = thisArticle.articleTitle;
+	newDiv.innerHTML += thisArticle.articleDescription;
+	newDiv.innerHTML += thisArticle.articleLink;
+
+	$("#output").append(newDiv);
+
+}	
 });
